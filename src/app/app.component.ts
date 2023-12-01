3856b7012bea18a4bb2ca552b03fb02340bc49c7
beyzarslann <beyzarslan865@gmail.com>
2023-12-01 22:39:18 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularCardProject';
+
+  posts = [
+    {
+      title: 'Camping',
+      imageUrl: '/assets/camping.jpg',
+      username: 'Camp',
+      content: 'Camping is a wonderful outdoor activity that allows people to connect with nature and escape the hustle and bustle of daily life. '
+    },
+    {
+      title: 'Climbing',
+      imageUrl: '/assets/paragliding.jpg',
+      username: 'Climb',
+      content: 'Climbing is an exhilarating adventure that combines physical challenge with the thrill of reaching new heights.'
+    },
+    {
+      title: 'Surfing',
+      imageUrl: '/assets/surf.jpg',
+      username: 'Surf',
+      content: 'Surfing is a dynamic water sport that involves riding ocean waves on a specially designed board. '
+    }
+
+  ];
+
+
+
 }
